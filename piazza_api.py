ada9586b706040bb62373edcd9d24069251914f4
Sumukh Sridhara <sumukh1@gmail.com>
2014-07-20 17:54:32 -0700

    First commit

---
+++ piazza_api.py
@@ -0,0 +1,130 @@
+import requests
+import json
+import getpass
+
+try: 
+    input = raw_input
+except NameError: 
+    raw_input = input
+    pass
+
+
+class AuthenticationError(Exception):
+    """AuthenticationError"""
+
+
+class NotAuthenticatedError(Exception):
+    """NotAuthenticatedError"""
+
+
+class PiazzaAPI(object):
+
+    """Tiny wrapper around Piazza's Internal API
+
+    Example:
+        >>> p = PiazzaAPI("hl5qm84dl4t3x2")
+        >>> p.user_auth()
+        Email: ...
+        Password: ...
+        >>> p.get(181)
+        ...
+    """
+
+    def __init__(self, network_id):
+        """
+        - Get user's password
+        - Authenticate with Piazza and get session cookie
+
+        :type  network_id: str
+        :param network_id: This is the ID of the network (or class) from which
+            to query posts
+        :type  email: str
+        "param email: Email address with which to authenticate (log in) with
+        """
+        self._nid = network_id
+        self.base_api_url = 'https://piazza.com/logic/api'
+        self.cookies = None
+
+    def user_auth(self, email=None, password=None):
+        """Login with email, password and get back a session cookie
+
+        :type  email: str
+        :param email: The email used for authentication
+        :type  password: str
+        :param password: The password used for authentication
+        """
+        email = raw_input("Email: ") if email is None else email
+        password = getpass.getpass() if password is None else password
+
+        login_url = self.base_api_url
+        login_data = {
+            "method": "user.login",
+            "params": {
+                "email": email,
+                "pass": password
+            }
+        }
+        login_params = {"method": "user.login"}
+        # If the user/password match, the server respond will contain a
+        #  session cookie that you can use to authenticate future requests.
+        r = requests.post(
+            login_url,
+            data=json.dumps(login_data),
+            params=login_params
+        )
+        if r.json()["result"] not in ["OK"]:
+            raise AuthenticationError(
+                "Could not authenticate.\n{}".format(r.json())
+            )
+        self.cookies = r.cookies
+
+    def demo_auth(self, auth=None, url=None):
+        """Authenticate with a "Share Your Class" URL using a demo user.
+
+        You may provide either the entire ``url`` or simply the ``auth`` parameter.
+
+        :param url: Example - "https://piazza.com/demo_login?nid=hbj11a1gcvl1s6&auth=06c111b"
+        :param auth: Example - "06c111b"
+        """
+        assert all([
+            auth or url,  # Must provide at least one
+            not (auth and url)  # Cannot provide more than one
+        ])
+        if url is None:
+            url = "https://piazza.com/demo_login"
+            params = dict(nid=self._nid, auth=auth)
+            res = requests.get(url, params=params)
+        else:
+            res = requests.get(url)
+        self.cookies = res.cookies
+
+    def get(self, cid, nid=None):
+        """Get data from post `cid` in network `nid`
+
+        :type  nid: str
+        :param nid: This is the ID of the network (or class) from which
+            to query posts. This is optional and only to override the existing
+            `network_id` entered when created the class
+        :type  cid: str|int
+        :param cid: This is the post ID which we grab
+        :returns: Python object containing returned data
+        """
+        if self.cookies is None:
+            raise NotAuthenticatedError("You must authenticate before making any other requests.")
+
+        nid = nid if nid else self._nid
+        content_url = self.base_api_url
+        content_params = {"method": "get.content"}
+        content_data = {
+            "method": "content.get",
+            "params": {
+                "cid": cid,
+                "nid": nid
+            }
+        }
+        return requests.post(
+            content_url,
+            data=json.dumps(content_data),
+            params=content_params,
+            cookies=self.cookies
+        ).json()
---
+++ search.py
@@ -0,0 +1,116 @@
+#Uses Python3 
+
+import json, re, html.parser
+from algoliasearch import algoliasearch
+import piazza_api as piazza
+import config 
+
+
+# Initialize API Client & Index
+client = algoliasearch.Client(config.aloglia_id, config.algolia_key)
+index = client.initIndex('CS61A Index')
+classid = config.classid
+
+p = piazza.PiazzaAPI(classid)
+p.user_auth()
+
+h = html.parser.HTMLParser()
+
+def striphtml(data):
+    p = re.compile(r'<.*?>')
+    return p.sub('', data)
+
+
+def html_decode(s):
+    """
+    Returns the ASCII decoded version of the given HTML string. This does
+    NOT remove normal HTML tags like <p>.
+    """
+    # htmlCodes = (('"', '&#34;'), ('-', '&#43;') ('"', '&amp;#34;'), ('@', '&amp;#64;') , (' ', '&nbsp;'), ("'", '&#39;'),('"', '&quot;'),('"', '&34;'),('>', '&gt;'),('<', '&lt;'),('&', '&amp;'))
+    # for code in htmlCodes:
+    #     s = s.replace(code[1], code[0])
+    return h.unescape(s)
+
+
+def process(start=6, end=1500):
+	array = []
+
+	# iterate over results and send them by batch of 10000 elements
+	for i in range(start,end):
+	    # select the identifier of this row
+		post = p.get(i)
+
+		if post['error'] != None or post['result'] == None:
+			pass
+		else: 
+			result = post['result']
+			row = {}
+
+			if 'data' in result:
+				if 'status' in result:
+					if result['status'] == "private":
+						pass
+
+			if 'status' in result:
+				if result['status'] == "private":
+					pass
+
+			row['objectID'] = i
+
+
+			row['qid'] = i
+			row['urlto'] = 'https://piazza.com/class/'+classid+'?cid='+str(i)
+
+			if 'tags' in result:
+				row['tags'] = result['tags']
+
+				if 'private' in result['tags']:
+					pass
+
+				if 'pin' in result['tags']:
+					row['pinned'] = 1
+				else:
+					row['pinned'] = 0
+
+			if 'history' in result:
+				row['postContent'] = html_decode(striphtml(html_decode(result['history'][0]['content'])))
+				row['subject'] = html_decode(result['history'][0]['subject'])
+
+	 		#Metrics
+			if 'tag_good_arr' in result:
+				row['good_q'] = len(result['tag_good_arr'])
+			if 'bookmarked' in result:
+				row['bookmarked'] = result['bookmarked']
+			if 'num_favorites' in result:
+				row['num_favorites'] = result['num_favorites']
+
+			if 'is_tag_good' in result:
+				if result['is_tag_good']:
+					row['is_tag_good'] = 1
+				else:
+					row['is_tag_good'] = 0
+
+			if 'children' in result: 
+				for elem in result['children']:
+					if 'subject' in elem:
+						if 'followups' in row:
+							row['followups'] += html_decode(striphtml(html_decode('\n' + elem['subject'])))
+						else:
+							row['followups'] = html_decode(striphtml(html_decode(elem['subject'])))
+					if 'history' in elem:
+						row['responses'] = html_decode(striphtml(html_decode(elem['history'][0]['content'])))
+
+			row['raw_data'] = result
+			print(i)
+
+			array.append(row)
+
+			if len(array) == 35:
+				index.saveObjects(array)
+				array = []
+				print("saving to search")
+
+
+	index.saveObjects(array)
+
+process()
